02359fb8c4cf07d268d3f5eb5e5596376bd7cc0f
Rainer Sieger <github@siegersoft.de>
2015-06-23 16:04:13 +0200

    Add CreateReferenceImportFile.cpp

---
+++ trunk/Sources/CreateReferenceImportFile.cpp
@@ -0,0 +1,207 @@
+/* FileIDConverter            */
+/* 2007-11-07                 */
+/* Dr. Rainer Sieger          */
+
+#include "Application.h"
+
+// **********************************************************************************************
+// **********************************************************************************************
+// **********************************************************************************************
+// 2008-01-20
+
+/*! @brief Liest den Record 0001 und speichert ihn in einer Datei.
+*
+*   @param FilenameIn Dateiname der Inputdatei
+*   @param Station_ptr Pointer auf Array aller Stationsnamen
+*   @param i_NumOfFiles Anzahl der Dateien
+*
+*   @return Fehlercode
+*/
+
+int MainWindow::CreateReferenceImportFile( const QString& s_FilenameIn, QStringList& sl_FilenameOut, structStaff *Staff_ptr, structStation *Station_ptr, const int i_NumOfFiles )
+{
+    int				i_Year			= 2000;
+    int				i_Month			= 1;
+    int				i_Day			= 1;
+    int				i_StationNumber	= 0;
+    int				i_PiID			= 506;
+
+    QString			InputStr		= "";
+
+    QString			s_StationName	= "";
+    QString			s_EventLabel	= "";
+
+    unsigned int	ui_length		= 1;
+    unsigned int	ui_filesize		= 1;
+
+    bool			b_Stop			= false;
+
+// ***********************************************************************************************************************
+
+    QFileInfo fi( s_FilenameIn );
+
+    if ( ( fi.exists() == false ) || ( fi.suffix().toLower() == "zip" ) || ( fi.suffix().toLower() == "gz" ) )
+        return( _FILENOEXISTS_ );
+
+// ***********************************************************************************************************************
+
+    QFile fin( s_FilenameIn );
+
+    if ( fin.open( QIODevice::ReadOnly | QIODevice::Text ) == false )
+        return( -10 );
+    ui_filesize = fin.size();
+
+// ***********************************************************************************************************************
+
+    QTextStream tin( &fin );
+
+// ***********************************************************************************************************************
+
+    initProgress( i_NumOfFiles, s_FilenameIn, tr( "Creating reference import file ..." ), 100 );
+
+// ***********************************************************************************************************************
+
+    InputStr  = tin.readLine();
+    ui_length = incProgress( i_NumOfFiles, ui_filesize, ui_length, InputStr );
+
+    if ( ( InputStr.startsWith( "*C0001" ) == false ) && ( InputStr.startsWith( "*U0001" ) == false ) )
+    {
+        resetProgress( i_NumOfFiles );
+        fin.close();
+        return( -40 );
+    }
+
+// ***********************************************************************************************************************
+
+    InputStr  = tin.readLine();
+    ui_length = incProgress( i_NumOfFiles, ui_filesize, ui_length, InputStr );
+
+    i_StationNumber	= InputStr.left( 3 ).toInt();
+    s_StationName	= findStationName( i_StationNumber, Station_ptr );
+    s_EventLabel	= findEventLabel( i_StationNumber, Station_ptr );
+
+// ***********************************************************************************************************************
+
+    i_Day   = 1;
+    i_Month	= InputStr.mid( 4, 2 ).toInt();
+    i_Year	= InputStr.mid( 7, 4 ).toInt();
+
+    QDateTime dt = QDateTime().toUTC();
+
+    dt.setDate( QDate( i_Year, i_Month, i_Day ) );
+    dt.setTime( QTime( 0, 0, 0 ) );
+
+// ***********************************************************************************************************************
+
+    if ( checkFilename( fi.fileName(), s_EventLabel, InputStr.mid( 4, 2 ), InputStr.mid( 9, 2 ) ) == false )
+    {
+        resetProgress( i_NumOfFiles );
+        fin.close();
+        return( -41 );
+    }
+
+// ***********************************************************************************************************************
+
+    QFile fout( fi.absolutePath() + "/" + s_EventLabel + "_" + dt.toString( "yyyy-MM" ) + "_refImp.txt" );
+
+    if ( fout.open( QIODevice::WriteOnly | QIODevice::Text ) == false )
+    {
+        resetProgress( i_NumOfFiles );
+        fin.close();
+        return( -20 );
+    }
+
+    QTextStream tout( &fout );
+
+    appendItem( sl_FilenameOut, fout.fileName() );
+
+// ***********************************************************************************************************************
+
+    while ( ( tin.atEnd() == false ) && ( ui_length != (unsigned int) _APPBREAK_ ) && ( b_Stop == false ) )
+    {
+        InputStr = tin.readLine();
+        ui_length = incProgress( i_NumOfFiles, ui_filesize, ui_length, InputStr );
+
+        if ( ( InputStr.startsWith( "*C0002" ) == true ) || ( InputStr.startsWith( "*U0002" ) == true ) )
+        {
+            // Station scientist
+            InputStr = tin.readLine();
+            ui_length	= incProgress( i_NumOfFiles, ui_filesize, ui_length, InputStr );
+
+            InputStr = tin.readLine();
+            ui_length	= incProgress( i_NumOfFiles, ui_filesize, ui_length, InputStr );
+
+            i_PiID = findPiId( InputStr.left( 38 ).simplified(), Staff_ptr );
+
+            b_Stop = true;
+        }
+
+        // Abort if first data record reached
+        if ( ( InputStr.startsWith( "*C0100" ) == true ) || ( InputStr.startsWith( "*U0100" ) == true ) )
+            b_Stop = true;
+    }
+
+    tout << tr( "Author(s)" ) << "\t" << tr( "Year" ) << "\t" << tr( "Title" ) << "\t" << tr( "URI" ) << endl;
+    tout << ReferenceImportFile( s_EventLabel, dt, i_PiID, s_StationName ) << endl;
+
+//---------------------------------------------------------------------------------------------------
+
+    resetProgress( i_NumOfFiles );
+
+    fin.close();
+    fout.close();
+
+    if ( ui_length == (unsigned int) _APPBREAK_ )
+        return( _APPBREAK_ );
+
+    return( _NOERROR_ );
+}
+
+// **********************************************************************************************
+// **********************************************************************************************
+// **********************************************************************************************
+// 2015-06-23
+
+/*! @brief Steuerung fuer Create reference import file */
+
+void MainWindow::doCreateReferenceImportFile()
+{
+    int		i				= 0;
+    int		err				= 0;
+    int		stopProgress	= 0;
+
+    QStringList sl_FilenameOut;
+
+// **********************************************************************************************
+
+    if ( existsFirstFile( gi_ActionNumber, gs_FilenameFormat, gi_Extension, gsl_FilenameList ) == true )
+    {
+        sl_FilenameOut.clear();
+
+        initFileProgress( gsl_FilenameList.count(), gsl_FilenameList.at( 0 ), tr( "Creating reference import file..." ) );
+
+        while ( ( i < gsl_FilenameList.count() ) && ( err == _NOERROR_ ) && ( stopProgress != _APPBREAK_ ) )
+        {
+            err = CreateReferenceImportFile( gsl_FilenameList.at( i ), sl_FilenameOut, g_Staff_ptr, g_Station_ptr,  gsl_FilenameList.count() );
+
+            stopProgress = incFileProgress( gsl_FilenameList.count(), ++i );
+        }
+
+        resetFileProgress( gsl_FilenameList.count() );
+
+        if ( err == _NOERROR_ )
+            err = concatenateFiles( gs_Path + "/" + "BSRN_RefImp.txt", sl_FilenameOut, tr( "Creating reference import file..." ), 1, true );
+    }
+    else
+    {
+        err = _CHOOSEABORTED_;
+    }
+
+// **********************************************************************************************
+
+    endTool( err, stopProgress, gi_ActionNumber, gs_FilenameFormat, gi_Extension, gsl_FilenameList, tr( "Done" ), tr( "Creating reference import file was canceled" ), false, false );
+
+    onError( err );
+}
+
+
