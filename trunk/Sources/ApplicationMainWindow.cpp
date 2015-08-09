cf6efef03c982ee1f572ab0dce3435cf69239c15
Rainer Sieger <rsieger@pangaea.de>
2015-08-09 22:53:50 +0200

    ProgressDialog->hide() added

--- trunk/Sources/ApplicationMainWindow.cpp
+++ trunk/Sources/ApplicationMainWindow.cpp
@@ -1635,7 +1635,10 @@ int MainWindow::incFileProgress( const int i_NumOfFiles, const int i_FileNumber 
 void MainWindow::resetFileProgress( const int i_NumOfFiles )
 {
     if ( i_NumOfFiles > 1 )
+    {
+        FileProgressDialog->hide();
         FileProgressDialog->reset();
+    }
 
     clearStatusMessage();
 
@@ -1797,6 +1800,7 @@ void MainWindow::resetProgress( const int i_NumOfFiles )
         break;
 
     case 1:
+        ProgressDialog->hide();
         ProgressDialog->reset();
         break;
 
