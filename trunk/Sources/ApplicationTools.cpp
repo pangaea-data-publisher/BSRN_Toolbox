40ca51c75bc726764dd773cae325212bccacbb8b
Rainer Sieger <rsieger@pangaea.de>
2016-04-14 10:12:09 +0200

    Data description block -> METADATA END

--- trunk/Sources/ApplicationTools.cpp
+++ trunk/Sources/ApplicationTools.cpp
@@ -702,7 +702,7 @@ QString MainWindow::CloseDataDescriptionHeader()
 
     s_OutputStr.append( "}" );
     s_OutputStr.append( eol );
-    s_OutputStr.append( "//" );
+    s_OutputStr.append( "// METAHEADER END" );
     s_OutputStr.append( eol );
 
     return( s_OutputStr );
